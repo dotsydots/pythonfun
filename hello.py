9476715c37873b431a46410225ed0f7dba873c48
fin <finlaybo@gmail.com>
2016-02-29 20:26:44 -0500

    init

---
+++ hello.py
@@ -0,0 +1,5 @@
+for x in xrange(1,10):
+   print "Finlay wants to watch TV for ", x, " minutes"
+   total = total + x
+
+ print "that's a total of ", total, "minutes!"py
---
+++ zodiac_animals.py
@@ -0,0 +1,9 @@
+zodiac={}
+zodiac = [
+	"rat", "hare", "dog",
+	"dragon", "monkey", "snake",
+	"pig", "rooster","horse","ox"
+	]
+	
+for x in range(0,11):
+	print zodiac[x]
